a3ee109a0ba4c3f80e2e38cdd6135f64ed13876a
Reshma <reshmashaik0910@gmail.com>
2025-07-08 19:27:54 +0100

    Added mongourl

--- jwt-backend/index.js
+++ jwt-backend/index.js
@@ -3,6 +3,10 @@ const {connectMongoDb} = require('./connect')
 const customerRouter = require('./routes/customer')
 const cookieParser = require("cookie-parser");
 const dotenv = require('dotenv');
+const mongoose = require('mongoose')
+
+// Your MongoDB URI (you can store this in a `.env` file for security)
+const mongoDbURL = process.env.MONGODB_URL; // or use the string directly
 dotenv.config();
 const app = express()
 const cors = require("cors")
@@ -13,28 +17,25 @@ const port = 5001;
     credentials: true,                // Allow cookies and credentials
 })) */
 
-    const allowedOrigins = process.env.FRONTEND_URL.split(",");
-
-    app.use(
-      cors({
-        origin: function (origin, callback) {
-          if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-          } else {
-            callback(new Error("Not allowed by CORS"));
-          }
-        },
-        credentials: true,
-      })
-    );
+const allowedOrigins = process.env.FRONTEND_URL.split(",");
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
     
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/customers',customerRouter)
-connectMongoDb("mongodb://127.0.0.1:27017/my-project")
+connectMongoDb(mongoDbURL)
 .then(()=>{console.log('mongodb connected')})
 .catch((err)=>console.log('mongo error',err))
 
